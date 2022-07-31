ebb18bae71c80c231b6c11bb3907b42593f43419
Alexis Paez <alexispaez280103@gmail.com>
2022-07-31 13:15:50 -0300

    header h2

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -7,7 +7,7 @@ const Header = () => {
     <section className="header-section">
       <div className="header-container">
         <h1>Hi, I’m <span>Alexis Paez<span className="last-letter"></span></span></h1>
-        <p>Front End Developer<span className="last-letter"></span></p>
+        <p>Front End Developer / UI Designer<span className="last-letter"></span></p>
         
         <Socials />
 
