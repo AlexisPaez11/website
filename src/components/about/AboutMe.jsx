91d18a52a4c1a79b9ddcc836f48ec37b3fda204c
Alexis Paez <alexispaez280103@gmail.com>
2022-08-24 02:52:21 -0300

    css modified

--- src/components/about/AboutMe.jsx
+++ src/components/about/AboutMe.jsx
@@ -6,7 +6,7 @@ const AboutMe = () => {
     <section className='aboutMe-section'>
         <div className='aboutMe-container'>
           <h2>SOBRE MÍ</h2>
-          <p>Soy un desarrollador Front End autodidacta y apasionado por la tecnología. Tengo una gran pasión por crear sitios web e interfaces de usuario intuitivas y dinámicas. También tengo un blog donde comparto contenido sobre desarrollo web y tecnología. Interesado en poder realizar proyectos freelance interesantes, como formar parte de un equipo de trabajo con gente positiva.</p>
+          <p>Desarrollador Front End autodidacta y apasionado por la tecnología. Tengo una gran pasión por crear sitios web e interfaces de usuario intuitivas y dinámicas. También tengo un blog donde comparto contenido sobre desarrollo web y tecnología.</p>
 
           <Skills />
 
