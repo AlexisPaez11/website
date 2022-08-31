d01874dd877d4380d06aa008a470bbbdf939a13c
Alexis Paez <alexispaez280103@gmail.com>
2022-08-31 07:28:30 -0300

    swiper button

--- src/components/article/H-Article.jsx
+++ src/components/article/H-Article.jsx
@@ -2,46 +2,18 @@ import React from 'react'
 import { Swiper, SwiperSlide } from 'swiper/react';
 import { Navigation } from "swiper";
 import { NavLink } from 'react-router-dom'
-import ArticleData from '../../components/article/ArticleData'
-
 import Article from './Article'
-
-// Icons Arrow
-// import { IoIosArrowBack } from 'react-icons/io'
-// import { IoIosArrowForward } from 'react-icons/io'
-
-// Icons
-// import { HiCode } from 'react-icons/hi' // CategoryDev
-// import { IoGitNetworkSharp } from 'react-icons/io5' // CategoryTech
+import ArticleData from '../../components/article/ArticleData'
 import { HiViewGridAdd } from 'react-icons/hi' // SeeMore
-
-// ArticleIMG
-// import DevToolsChrome from '../../assets/blog-assets/devtools.webp'
-// import HTML from '../../assets/blog-assets/html.webp'
-// import MacBookAirM2 from '../../assets/blog-assets/macbook-air-m2.webp'
-// import MejoresRatonesGaming2022 from '../../assets/blog-assets/mejores-ratones-gaming-2022.webp'
-// import CSS from '../../assets/blog-assets/css.webp'
-// import JavaScript from '../../assets/blog-assets/javascript.webp'
-// import React from '../../assets/blog-assets/react.webp'
-
 
 const Articles = () => {
   return (
     <section className='h-articles'>
       <div className="projects-top">
         <h2>BLOG</h2>
-        {/* <div className='arrow-buttons'>
-          <div className="arrow-btn swiper-button-prev-projects">
-              <IoIosArrowBack />
-          </div>
-          <div className="arrow-btn swiper-button-next-projects">
-              <IoIosArrowForward />
-          </div>
-        </div> */}
       </div>
 
       <Swiper slidesPerView={'auto'} navigation={true} modules={[Navigation]} className="swiper">
-
         {
           ArticleData.slice(0,6).map(({categoryimg, category, image, title, date, url}) => {
             return (
--- src/components/project/H-Project.jsx
+++ src/components/project/H-Project.jsx
@@ -3,32 +3,19 @@ import { Swiper, SwiperSlide } from 'swiper/react';
 import { Navigation } from "swiper";
 import { NavLink } from 'react-router-dom'
 import Project from './Project'
-// import { IoIosArrowBack } from 'react-icons/io'
-// import { IoIosArrowForward } from 'react-icons/io'
+import ProjectData from './ProjectData'
 import { HiViewGridAdd } from 'react-icons/hi'
-import { SiHtml5, SiCss3, SiJavascript, SiReact } from 'react-icons/si'
-import YoutubeClone from '../../assets/projects-assets/youtube-clone.webp'
-import TwitterClone from '../../assets/projects-assets/twitter-clone.webp'
-import CarRental from '../../assets/projects-assets/car-rental.webp'
 
 const Projects = () => {
   return (
     <section className='h-projects'>
       <div className="projects-top">
         <h2>PROYECTOS</h2>
-        {/* <div className='arrow-buttons'>
-          <div className="arrow-btn swiper-button-prev-projects">
-              <IoIosArrowBack />
-          </div>
-          <div className="arrow-btn swiper-button-next-projects">
-              <IoIosArrowForward />
-          </div>
-        </div> */}
       </div>
 
       <Swiper slidesPerView={'auto'} navigation={true} modules={[Navigation]} className="swiper">
           {
-            ProjectData.slice(0,3).map(({id, image, title, technologies, github, demo}) => {
+            ProjectData.slice(0,6).map(({id, image, title, technologies, github, demo}) => {
               return (
                 <SwiperSlide>
                   <Project key={id} image={image} title={title} technologies={technologies} github={github} demo={demo} />
@@ -43,29 +30,5 @@ const Projects = () => {
     </section>
   )
 }
-
-const ProjectData = [
-  {
-    image: YoutubeClone,
-    title: 'Youtube Clone',
-    technologies: [<SiHtml5 />, <SiCss3 />, <SiJavascript />],
-    github: 'https://github.com/AlexisPaez11/Youtube',
-    demo: 'https://alexispaez11.github.io/Youtube/',
-  },
-  {
-    image: TwitterClone,
-    title: 'Twitter Clone',
-    technologies: [<SiHtml5 />, <SiCss3 />],
-    github: 'https://github.com/AlexisPaez11/Twitter',
-    demo: 'https://alexispaez11.github.io/Twitter/',
-  },
-  {
-    image: CarRental,
-    title: 'Car Rental',
-    technologies: [<SiReact />, <SiCss3 />],
-    github: 'https://github.com/AlexisPaez11/Car-Rental',
-    demo: 'https://optimus-alexispaez11.vercel.app/',
-  },
-]
 
 export default Projects
