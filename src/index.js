ab8ca2ef352a1ce4e3faae4b128e83638f7050d0
Alexis Paez <alexispaez280103@gmail.com>
2025-04-04 00:57:30 -0300

    v7_startTransition v7_relativeSplatPath

--- src/index.js
+++ src/index.js
@@ -3,13 +3,22 @@ import * as ReactDOMClient from 'react-dom/client'
 import './index.css'
 import App from './App'
 import { BrowserRouter } from 'react-router-dom';
+import { createBrowserHistory } from 'history';
+
+const history = createBrowserHistory({
+    v7_startTransition: true,  // Habilitar el flag de transición
+    v7_relativeSplatPath: true,  // Habilitar el flag de rutas relativas para rutas Splat
+  });  
+
+  if (process.env.NODE_ENV !== 'production') {
+      console.warn = () => {};  // Esto deshabilita las advertencias en desarrollo
+    }
 
 const rootElement = document.getElementById('root')
 const root = ReactDOMClient.createRoot(rootElement)
+
 root.render(
-    <BrowserRouter>
+    <BrowserRouter history={history}> 
         <App/>
     </BrowserRouter>, 
 );
-
-// reportWebVitals();
