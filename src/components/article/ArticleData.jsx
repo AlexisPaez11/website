e61f27f598a9abfd5b1dcedd5b8bff7c6f1aef71
Alexis Paez Carrazan <77308122+AlexisPaez11@users.noreply.github.com>
2025-09-29 23:12:38 -0300

    Update ArticleData.jsx

--- src/components/article/ArticleData.jsx
+++ src/components/article/ArticleData.jsx
@@ -133,10 +133,10 @@ const ArticleData = [
 // APPLE
     {
     field: ['Apple', 'Apple Intelligence'],
-    category: ['iPhone'], 
+    category: ['iPhone'],
     image: '/assets/blog-assets/iPhone17Pro/iPhone17Pro.webp',
     title: 'iPhone 17 Pro y Pro Max: Poder sin limites',
-    date:'Septiembre 26, 2025',
+    date:'Septiembre 27, 2025',
     url: 'AweDroppingiPhone17Pro',
     },
     {
